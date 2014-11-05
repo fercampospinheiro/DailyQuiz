4996511d423f23334ea4e52c9de545127e7338e3
fernando <sueli2011>
2014-11-04 23:10:42 -0200

    3º commit 4ª entrega

--- docs/scripts/create_database_tables.sql
+++ docs/scripts/create_database_tables.sql
@@ -3,7 +3,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE SCHEMA IF NOT EXISTS `dailyquiz` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+USE `dailiquiz` ;
 
 -- -----------------------------------------------------
 -- Table `dailiquiz`.`Questionario`
