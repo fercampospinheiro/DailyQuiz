8e01c5c5d48c1919e8e82398d2b2504d74c3b3c8
fernando <sueli2011>
2015-02-12 00:04:07 -0200

    Atualização dos Teste dos metódos de dias para proximo aniversario e faz aniversario hoje

--- src/main/java/br/com/sidlar/dailyquiz/domain/membro/Membro.java
+++ src/main/java/br/com/sidlar/dailyquiz/domain/membro/Membro.java
@@ -3,7 +3,6 @@ package br.com.sidlar.dailyquiz.domain.membro;
 import org.hibernate.annotations.Type;
 import org.joda.time.*;
 import javax.persistence.*;
-import static org.joda.time.Days.*;
 
 /**
  * Membro da aplicação
@@ -19,12 +18,10 @@ public class Membro {
 	private String senha;
 	@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDate")
 	private LocalDate dataNascimento;
+	@Transient
+	private LocalDate dataAtual = LocalDate.now();
 
-
-
-	public Membro() {
-
-	}
+	public Membro() {}
 
     public Membro(String email, String nome) {
         this.email = email;
@@ -38,15 +35,6 @@ public class Membro {
 	public Years getIdade(){
 		return  Years.yearsBetween(dataNascimento,LocalDate.now());
 	}
-
-	public boolean isAniversarioHoje(){
-		int diaAtual = LocalDate.now().getDayOfMonth();
-        int mesAtual = LocalDate.now().getMonthOfYear();
-        int diaDoAniversario = this.dataNascimento.getDayOfMonth();
-        int mesDoAniversario = this.dataNascimento.getMonthOfYear();
-
-        return (diaDoAniversario == diaAtual) && (mesDoAniversario == mesAtual);
-    }
 
 	public String getNome() {
 		return nome;
@@ -64,10 +52,6 @@ public class Membro {
 		this.email = email;
 	}
 
-	public String getSenha() {
-		return senha;
-	}
-
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
@@ -75,10 +59,6 @@ public class Membro {
 	public boolean possuiSenhaInformada(String senha){
 		return	this.senha.equals(senha);
 	}
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
 
     
     public Integer getId() {
@@ -109,24 +89,33 @@ public class Membro {
 		dataNascimento = data;
 	}
 
-	public boolean fazAniversarioHoje(LocalDate dataAtual) {
+	public boolean fazAniversarioHoje() {
 		if(dataAtual.getYear() == dataNascimento.getYear()) return false;
 		MonthDay diaEMesAtual =  new MonthDay(dataAtual);
 		MonthDay diaEMesDoNascimento = new MonthDay(dataNascimento);
 		return diaEMesAtual.equals(diaEMesDoNascimento);
 	}
 
-	public Days faraAniversarioEm(LocalDate proximoAniversario) {
+	public Days obtemDiasParaAniversario() {
+		LocalDate  proximoAniversario = obtemProximoAniversario();
+		return Days.daysBetween(dataAtual, proximoAniversario);
+	}
 
-		int diaDoNascimento = dataNascimento.getDayOfMonth();
-		int mesDoNascimento = dataNascimento.getMonthOfYear();
-		int anoUltimoAniversario = LocalDate.now().getYear();
+	private LocalDate obtemProximoAniversario(){
 
-		if(proximoAniversario.getYear() == anoUltimoAniversario )
-			anoUltimoAniversario--;
+		MonthDay diaMesAtual = new MonthDay(dataAtual.getMonthOfYear(),dataAtual.getDayOfMonth());
+		MonthDay diaMesAniversario = new MonthDay(dataNascimento.getMonthOfYear(),dataNascimento.getDayOfMonth());
 
-		LocalDate  aniversarioAnterior = new LocalDate(diaDoNascimento,mesDoNascimento,anoUltimoAniversario);
+		int anoAniversario = dataAtual.getYear();
 
-		return Days.daysBetween(aniversarioAnterior, proximoAniversario);
+		if(diaMesAniversario.isBefore(diaMesAtual))
+			/*Aniversario passou busca próximo ano*/
+			anoAniversario++;
+
+		return new LocalDate(anoAniversario,diaMesAniversario.getMonthOfYear(),diaMesAniversario.getDayOfMonth());
+	}
+
+	public void setDataAtual(LocalDate dataAtual) {
+		this.dataAtual = dataAtual;
 	}
 }
