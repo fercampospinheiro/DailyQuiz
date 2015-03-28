0ef9b46612a35a7bac69443fd339985027e7ec52
fernando <sueli2011>
2015-03-28 17:21:32 -0300

    Atualizacao das funçoes javascript

--- src/main/webapp/resources/js/novoQuestionario.js
+++ src/main/webapp/resources/js/novoQuestionario.js
@@ -1,13 +1,44 @@
-    $("#ok").click(
 
-            function (){
-                var novaAlternativa,atributo,descricaoAtributo;
-                atributo = "path";
-                descricaoAtributo = "questoes[0].alternativas[0].descricao";
+var i = 0;
 
-                novaAlternativa =  $(".alternativa").clone();
-                novaAlternativa.attr(atributo,descricaoAtributo);
-                novaAlternativa.appendTo(".alternativas");
-          
+$(
+
+    $("#adicionaAlternativa").click(function(){
+
+
+      function geraEstrutura(){
+        i = i +1
+        $('.alternativas').append(novaAlternativa())
+      }
+
+      function novaAlternativa(){
+
+        return $(
+            '<div class = "alternativa"+"i">'+
+
+                '<label >'+
+                  i +  ' ordem:'+'<a href ="javascript:removeAlternativa('+i+');" class = "btn btn-default btn-xs">x</a>'+
+                '</label>'+
+                '<input path="questoes[0].alternativas['+i+'].ordem" class= "form-control"/>'+
+
+                '<label>'+
+                    'descricao:'+
+                '</label>'+
+                '<input path="questoes[0].alternativas['+i+'].descricao" class="form-control"/>'+
+            '</div>'
+            );
+      }
+
+      geraEstrutura();
+
+      }
+
+        )
+
+  )
+
+      function removeAlternativa(numero){
+
+          $('.alternativa'+numero).hide();
+
             }
-    )
