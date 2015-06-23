2ab333eb4a9baa093a21ed4196403d82eb44bb4a
Fernando de Campos Pinheiro <fercampospinheiro@gmail.com>
2015-06-23 14:24:33 -0300

    Inserção de controller cadastro questionario - post

--- src/main/java/br/com/sidlar/dailyquiz/presentation/cadastroQuestionario/CadastroQuestionarioController.java
+++ src/main/java/br/com/sidlar/dailyquiz/presentation/cadastroQuestionario/CadastroQuestionarioController.java
@@ -2,6 +2,7 @@ package br.com.sidlar.dailyquiz.presentation.cadastroQuestionario;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,5 +17,10 @@ public class CadastroQuestionarioController {
 		return "/CadastroQuestionario/novoCadastroQuestionario";
 	}
 	
-	
+	@RequestMapping(value ="/novo",method = RequestMethod.POST)
+	public String cadastraQuestionario(@ModelAttribute("formulario")FormularioCadastroQuestionario formulario){
+		return "/CadastroQuestionario/novoCadastroQuestionario";
+	}
+
+
 }
